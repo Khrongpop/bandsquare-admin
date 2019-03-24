31c78a67ce0f7d1fbf71adfef34d56bb5aa20d5e
Khrongpop <mr.khrongpop@gmail.com>
2019-03-24 16:57:25 +0700

    deploy

--- server/app.js
+++ server/app.js
@@ -4,14 +4,14 @@ const path = require('path');
 const app = require('./route');
 
 //production mode
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../build')));
-    //
-    app.get('*', (req, res) => {
-      res.sendfile(path.join(__dirname = '../build/index.html'));
-    })
-  }
-  else {
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../build')));
+//     //
+//     app.get('*', (req, res) => {
+//       res.sendfile(path.join(__dirname = '../build/index.html'));
+//     })
+//   }
+//   else {
 
         // Server static assets
         app.use(express.static(path.resolve(__dirname, '..', 'build')));
@@ -19,6 +19,6 @@ if(process.env.NODE_ENV === 'production') {
         app.get('*', (req, res) => {
             res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
         });
-  }
+//   }
 
 module.exports = app;
